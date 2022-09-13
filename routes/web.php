136f50e8c235c2f61646e3aee3487128161baa17
Suketi17 <nguyenduyhau.dct@gmail.com>
2022-09-13 20:10:38 +0700

    ok

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\MainController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Users\LoginController;
 
@@ -20,3 +21,6 @@ use App\Http\Controllers\Admin\Users\LoginController;
 
 
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/admin/users/login/store', [LoginController::class, 'store']);
+
+Route::get('admin/main', [MainController::class, 'index']) -> name('admin');
